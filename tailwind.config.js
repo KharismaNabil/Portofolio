0f49c6d490691825550f6a47bf75c1c80ebe3a49
KharismaNabil <63237451+KharismaNabil@users.noreply.github.com>
2022-05-04 18:19:14 +0700

    Banner section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,15 @@ module.exports = {
   content: ["index.html"],
   theme: {
     extend: {
+       container: {
+          center: true,
+          padding: "20px",
+       },
        colors: {
           primay: '#F66B0E',
+       },
+       screen:{
+          '2xl' : '1320px',
        }
     },
   },
